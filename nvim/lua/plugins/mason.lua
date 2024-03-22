996d184de560d5647dd6550d713740f6b8ff6fd7
yugen <yugen.m7@gmail.com>
2024-03-22 08:47:01 +0545

    added border to mason

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return{
+
+"mason.nvim",
+    opts = {
+      ui = {
+        border = 'single',
+        scrollbar = false,
+        winhighlight = "Normal:None,FloatBorder:None"
+      },
+    },
+
+}
